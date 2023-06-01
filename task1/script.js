7daff84c55d60f0dbdd1418b60e1c87858ec09ff
Pranjal Jain <pranjaljain275@gmail.com>
2023-06-01 12:48:41 +0530

    each book section page added

--- task1/script.js
+++ task1/script.js
@@ -1,31 +1,48 @@
-const bookURL = "https://yif-library-be.onrender.com/book"
+const bookURL = "https://yif-library-be.onrender.com/book";
 
 const bookSection = document.querySelector(".viewBook");
 
+let booksData = [];
+
 async function getBook() {
-    try {
-        let res = await fetch(bookURL);
-        let data = await res.json();
-        console.log(data);
-        renderData(data);
-    } catch (err) {
-        console.log(err.message);
-    }
+  try {
+    let res = await fetch(bookURL);
+    let data = await res.json();
+    booksData = data;
+    renderData(data);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 getBook();
 
 function renderData(books) {
-    bookSection.innerHTML = "";
+  bookSection.innerHTML = "";
 
-    bookSection.innerHTML = `
-        ${books.map((el,index)=>{
+  bookSection.innerHTML = `
+        ${books
+          .map((el, index) => {
             return `
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLqbd5hH6jYX37N6BCQIIM5DqASiVkQ-AN1ZtSsxIi-n7qVYkFBEF93F976EsbGuRrpew&usqp=CAU" alt="">
-                <h4><strong>Title</strong> : ${el.bookName}</h4>
-                <h4><strong>Author</strong> : ${el.authorName}</h4>
+            <div class="eachBook" data-id=${el._id}>
+                <img data-id=${el._id} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLqbd5hH6jYX37N6BCQIIM5DqASiVkQ-AN1ZtSsxIi-n7qVYkFBEF93F976EsbGuRrpew&usqp=CAU" alt="">
+                <h4 data-id=${el._id}><strong data-id=${el._id}>Title</strong> : ${el.bookName}</h4>
+                <h4 data-id=${el._id}><strong data-id=${el._id}>Author</strong> : ${el.authorName}</h4>
             </div>
-            `
-        }).join(" ")}
-    `
+            `;
+          })
+          .join(" ")}
+    `;
+
+  let allbook = document.querySelectorAll(".eachBook");
+  for (let book of allbook) {
+    book.addEventListener("click", (event) => {
+      let ids = event.target.dataset.id;
+      let eachdata = booksData.filter((el, index) => {
+        return el._id == ids;
+      });
+
+      localStorage.setItem("book", JSON.stringify(eachdata));
+      window.location.href = "bookview.html";
+    });
+  }
 }
